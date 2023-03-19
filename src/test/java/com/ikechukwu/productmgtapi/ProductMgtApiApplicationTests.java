23fc5d17737324c2fc7abf224e3194faf1679356
Ikechukwu Iwunna <89745907+Manuel1407@users.noreply.github.com>
2023-03-19 14:03:52 +0100

    added starter code for test

---
+++ src/test/java/com/ikechukwu/productmgtapi/ProductMgtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikechukwu.productmgtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMgtApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
